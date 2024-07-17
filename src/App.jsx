ddb46d3bd08c1694ffd4f0c76a6931203e41526f
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-07-17 06:34:50 -0700

    added counter and correctly displyed the puppy list

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
+import { puppyList } from './data'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Counter() {
+  const [count, setCount] = useState(0);
+console.log(puppyList)
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
-  )
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default Counter;
