9b5f8414f9c99a0e25c96e54633585a4fcab9be3
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-08-05 12:08:18 -0700

    fixed random page showing up and not having any code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import { puppyList } from './data';
+import { puppyList } from './data.js';
 import React, { useState } from 'react';
-
+import './App.css';
 
 const Listpups = () => {
-  
+  const [selectedPuppy, setSelectedPuppy] = useState(null);
 
+  const handleClick = (puppy) => {
+    setSelectedPuppy(puppy);
+  };
 
-  console.log(puppyList)
   return (
-    <>
-      <div>
-        <h1>puppy List</h1>
-        
-      </div>
-    </>
+    <div className="container">
+      <h1>Puppy List</h1>
+      <ul>
+        {puppyList.map((puppy) => (
+          <li key={puppy.id} onClick={() => handleClick(puppy)}>
+            {puppy.name}
+          </li>
+        ))}
+      </ul>
+      {selectedPuppy && (
+        <div className="selected-puppy">
+          <h1>{selectedPuppy.name}</h1>
+          <p>Age: {selectedPuppy.age}</p>
+          <p>Breed: {selectedPuppy.breed}</p>
+          <p>Email: {selectedPuppy.email}</p>
+          <p>Is Cute: {selectedPuppy.isCute ? "Yes" : "No"}</p>
+          <h2>Tricks:</h2>
+          <ul>
+            {selectedPuppy.tricks.map((trick) => (
+              <li key={trick.id}>{trick.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
   );
-}
-
+};
 
 export default Listpups;
